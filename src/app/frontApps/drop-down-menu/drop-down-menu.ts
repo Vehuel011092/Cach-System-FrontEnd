76c03935dd58798b74e94a633012c033612ca4b0
Xenovia01 <angel.antonio.rios.picaso@gmail.com>
2026-02-12 14:25:05 -0800

    Nuevas Caracteristicas

--- src/app/frontApps/drop-down-menu/drop-down-menu.ts
+++ src/app/frontApps/drop-down-menu/drop-down-menu.ts
@@ -1,6 +1,11 @@
-import { Component, viewChild } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import {Menu, MenuContent, MenuItem, MenuTrigger} from '@angular/aria/menu';
 import {OverlayModule} from '@angular/cdk/overlay'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { ConnectedPosition } from '@angular/cdk/overlay';
+
 
 @Component({
   selector: 'app-drop-down-menu',
@@ -12,4 +17,29 @@ export class DropDownMenu {
   formatMenu = viewChild<Menu<string>>('formatMenu');
   categorizeMenu = viewChild<Menu<string>>('categorizeMenu');
 
+    private breakpointObserver = inject(BreakpointObserver);
+  
+  isMobile = toSignal(
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait])
+      .pipe(map(result => result.matches)),
+    { initialValue: false }
+  );
+
+  desktopPositions: ConnectedPosition[] = [
+    { originX: 'start', originY: 'bottom', overlayX: 'start', overlayY: 'top', offsetY: 4 },
+    { originX: 'start', originY: 'top', overlayX: 'start', overlayY: 'bottom', offsetY: -4 },
+    { originX: 'end', originY: 'bottom', overlayX: 'end', overlayY: 'top', offsetY: 4 },
+    { originX: 'end', originY: 'top', overlayX: 'end', overlayY: 'bottom', offsetY: -4 },
+    { originX: 'center', originY: 'bottom', overlayX: 'center', overlayY: 'top', offsetY: 4 }
+  ];
+
+  mobilePositions: ConnectedPosition[] = [
+    { originX: 'center', originY: 'bottom', overlayX: 'center', overlayY: 'top', offsetY: 8 },
+    { originX: 'center', originY: 'top', overlayX: 'center', overlayY: 'bottom', offsetY: -8 }
+  ];
+
+  get overlayPositions(): ConnectedPosition[] {
+    return this.isMobile() ? this.mobilePositions : this.desktopPositions;
+  }
 }
