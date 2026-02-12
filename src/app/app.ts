e07f5aea4fd58ab583d78aad392280d17a63b789
Xenovia01 <angel.antonio.rios.picaso@gmail.com>
2026-02-11 16:22:20 -0800

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DropDownMenu } from "./frontApps/drop-down-menu/drop-down-menu";
@@ -6,24 +5,14 @@ import { DropDownMenu } from "./frontApps/drop-down-menu/drop-down-menu";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, DropDownMenu],
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
->>>>>>> 0acc1b9450738ec767cdd83905651d47da3f6f8f
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-<<<<<<< HEAD
   protected readonly title = signal('Cach-System-FrontEnd');
 
   reload() {
     window.location.reload();
   }
 
-=======
-  protected readonly currentYear = new Date().getFullYear();
->>>>>>> 0acc1b9450738ec767cdd83905651d47da3f6f8f
 }
